c5282bb4b8879d3b991d52248771970c15a7cf80
Li Gang <gang.g.li@intel.com>
2012-03-27 15:28:08 +0800

    [PORT FROM MAIN]Tethering Setting: Fix stange/unstable when disable/enable RNDIS while USB Accessory connected
    BZ: 25097
    
    when getting ACCESSORY=START" uevent, disable tethering
    
    Change-Id: I7b7b2249e28b567d3720778d50828271d1c09061
    Orig-Change-Id: I78bd4b2511abfef59095a8e9537d26080bef9c5e
    Category: aosp improvement
    Domain: USB Device
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Li Gang <gang.g.li@intel.com>

--- src/com/android/settings/TetherSettings.java
+++ src/com/android/settings/TetherSettings.java
@@ -95,6 +95,7 @@ public class TetherSettings extends SettingsPreferenceFragment
 
     private boolean mUsbConnected;
     private boolean mMassStorageActive;
+    private boolean mAccessoryMode;
 
     private boolean mBluetoothEnableForTether;
 
@@ -233,6 +234,7 @@ public class TetherSettings extends SettingsPreferenceFragment
                 updateState();
             } else if (action.equals(UsbManager.ACTION_USB_STATE)) {
                 mUsbConnected = intent.getBooleanExtra(UsbManager.USB_CONNECTED, false);
+                mAccessoryMode = intent.getBooleanExtra(UsbManager.USB_FUNCTION_ACCESSORY, false);
                 updateState();
             } else if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
                 if (mBluetoothEnableForTether) {
@@ -326,7 +328,7 @@ public class TetherSettings extends SettingsPreferenceFragment
             String[] errored) {
         ConnectivityManager cm =
                 (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        boolean usbAvailable = mUsbConnected && !mMassStorageActive;
+        boolean usbAvailable = mUsbConnected && !mMassStorageActive && !mAccessoryMode;
         int usbError = ConnectivityManager.TETHER_ERROR_NO_ERROR;
         for (String s : available) {
             for (String regex : mUsbRegexs) {
@@ -368,6 +370,10 @@ public class TetherSettings extends SettingsPreferenceFragment
             mUsbTether.setChecked(false);
         } else if (mMassStorageActive) {
             mUsbTether.setSummary(R.string.usb_tethering_storage_active_subtext);
+            mUsbTether.setEnabled(false);
+            mUsbTether.setChecked(false);
+        } else if (mAccessoryMode) {
+            mUsbTether.setSummary(R.string.usb_tethering_accessory_active_subtext);
             mUsbTether.setEnabled(false);
             mUsbTether.setChecked(false);
         } else {
