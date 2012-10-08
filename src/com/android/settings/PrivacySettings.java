19769d5b37e40ae3e0f162c9493481b6d18d2a38
jshe32X <jianchunx.shen@intel.com>
2012-10-08 11:02:12 +0800

    [PORT FROM MAIN] Settings: Fix KW issue in packages/apps/Settings/src/com/android/settings/PrivacySettings.java
    BZ: 60764
    
    RootCause: add mBackupManager != null before using mBackupManager
    
    Change-Id: Icf70bc41a3dd37d07ae16a520be70976a54e4574
    Signed-off-by: jshe32X<jianchunx.shen@intel.com>
    Reviewed-on: http://android.intel.com:8080/75976
    Reviewed-by: lab_cactus <lab_cactus@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- src/com/android/settings/PrivacySettings.java
+++ src/com/android/settings/PrivacySettings.java
@@ -201,14 +201,18 @@ public class PrivacySettings extends SettingsPreferenceFragment implements
         }
         mBackup.setChecked(enable);
         mAutoRestore.setEnabled(enable);
-        try {
-            String transport = mBackupManager.getCurrentTransport();
-            Intent configIntent = mBackupManager.getConfigurationIntent(transport);
-            if ((!enable) || (enable && configIntent != null)) {
-                mConfigure.setEnabled(enable);
-            }
-        } catch (RemoteException e) {
+        if (mBackupManager != null) {
+            try {
+                String transport = mBackupManager.getCurrentTransport();
+                Intent configIntent = mBackupManager.getConfigurationIntent(transport);
+                if ((!enable) || (enable && configIntent != null)) {
+                    mConfigure.setEnabled(enable);
+                }
+            } catch (RemoteException e) {
             //To do Nothing
+            }
+        } else {
+            mConfigure.setEnabled(enable);
         }
     }
 
