62a54fdb4cd66607943b666300b5eadc92ce649c
Matt Gumbel <matthew.k.gumbel@linux.intel.com>
2012-10-18 17:45:16 -0700

    REVERTME Add Ethernet settings panel
    This will add a new Ethernet panel to Settings menu and
    also display a switch to enable/disable whether to use
    Ethernet or not. The panel enables a user to configure
    both dhcp and static IP settings.
    
    Author: Matt Gumbel <matthew.k.gumbel@linux.intel.com>
    Author: Anton Cherkashyn <antonx.t.cherkashyn@intel.com>
    Author: juntingwang <Junting.Wang@windriver.com>
    Author: Constantin Musca <constantin.musca@intel.com>
    Author: Markus Grape <markus.grape@intel.com>
    
    Category: AOSP improvement
    Domain: AOSP.Connectivity
    Origin: internal
    Upstream-Candidate: yes, requires rework
    
    Change-Id: Ie7fba15d330bdface4fdd0d409c9313a989ba97f
    Signed-off-by: Markus Grape <markus.grape@intel.com>
    Signed-off-by: sahashau <shaunak.saha@intel.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -40,6 +40,7 @@ import android.os.Bundle;
 import android.os.INetworkManagementService;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.Preference;
@@ -73,6 +74,7 @@ import com.android.settings.bluetooth.BluetoothEnabler;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.deviceinfo.Memory;
 import com.android.settings.deviceinfo.UsbSettings;
+import com.android.settings.ethernet.EthernetEnabler;
 import com.android.settings.fuelgauge.PowerUsageSummary;
 import com.android.settings.inputmethod.InputMethodAndLanguageSettings;
 import com.android.settings.inputmethod.KeyboardLayoutPickerFragment;
@@ -101,6 +103,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+
 
 /**
  * Top-level settings activity to handle single pane and double pane UI layout.
@@ -143,6 +146,7 @@ public class Settings extends PreferenceActivity
             R.id.data_usage_settings,
             R.id.wireless_settings,
             R.id.device_section,
+            R.id.ethernet_settings,
             R.id.sound_settings,
             R.id.display_settings,
             R.id.storage_settings,
@@ -579,6 +583,13 @@ public class Settings extends PreferenceActivity
                 if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
                     target.remove(i);
                 }
+            } else if (id == R.id.ethernet_settings) {
+                //Remove Ethernet Settings if Ethernet service is not available.
+                String ethernetProp = SystemProperties.get("ro.intel.ethernet", "false");
+                if (getSystemService(Context.ETHERNET_SERVICE) == null ||
+                    !ethernetProp.equals("true")) {
+                    target.remove(header);
+                }
             } else if (id == R.id.data_usage_settings) {
                 // Remove data usage when kernel module not enabled
                 final INetworkManagementService netManager = INetworkManagementService.Stub
@@ -820,6 +831,7 @@ public class Settings extends PreferenceActivity
 
         private final WifiEnabler mWifiEnabler;
         private final BluetoothEnabler mBluetoothEnabler;
+        private final EthernetEnabler mEthernetEnabler;
         private AuthenticatorHelper mAuthHelper;
         private DevicePolicyManager mDevicePolicyManager;
 
@@ -837,7 +849,9 @@ public class Settings extends PreferenceActivity
         static int getHeaderType(Header header) {
             if (header.fragment == null && header.intent == null) {
                 return HEADER_TYPE_CATEGORY;
-            } else if (header.id == R.id.wifi_settings || header.id == R.id.bluetooth_settings) {
+            } else if (header.id == R.id.wifi_settings ||
+                    header.id == R.id.bluetooth_settings ||
+                    header.id == R.id.ethernet_settings) {
                 return HEADER_TYPE_SWITCH;
             } else if (header.id == R.id.security_settings) {
                 return HEADER_TYPE_BUTTON;
@@ -884,6 +898,7 @@ public class Settings extends PreferenceActivity
             mWifiEnabler = new WifiEnabler(context, new Switch(context));
             mBluetoothEnabler = new BluetoothEnabler(context, new Switch(context));
             mDevicePolicyManager = dpm;
+            mEthernetEnabler = new EthernetEnabler(context, new Switch(context));
         }
 
         @Override
@@ -952,8 +967,10 @@ public class Settings extends PreferenceActivity
                     // Would need a different treatment if the main menu had more switches
                     if (header.id == R.id.wifi_settings) {
                         mWifiEnabler.setSwitch(holder.switch_);
-                    } else {
+                    } else if(header.id == R.id.bluetooth_settings) {
                         mBluetoothEnabler.setSwitch(holder.switch_);
+                    } else if (header.id == R.id.ethernet_settings) {
+                        mEthernetEnabler.setSwitch(holder.switch_);
                     }
                     updateCommonHeaderView(header, holder);
                     break;
@@ -1027,11 +1044,13 @@ public class Settings extends PreferenceActivity
         public void resume() {
             mWifiEnabler.resume();
             mBluetoothEnabler.resume();
+            mEthernetEnabler.resume();
         }
 
         public void pause() {
             mWifiEnabler.pause();
             mBluetoothEnabler.pause();
+            mEthernetEnabler.pause();
         }
     }
 
