18f55fc63ca0a6159ed79d9477c68c5e371e7b1f
Matthieu Mauger <matthieux.mauger@intel.com>
2013-04-09 10:50:02 +0200

    [PORT FROM R42S] WiFi Settings: hotspot vibration option saved only when Save button clicked
    BZ: 98349
    
    Recently, a new check-box option “user option to enable vibration when hotspot
    ready” has been added (patch 96752). However, the option state was saved
    immediately after clicking on the check-box rather than waiting the user to
    click on “Save” button.
    
    This patch saves the option state in the “Save” button listener.
    
    Change-Id: I43bf294467d494cb3ea7f10fb509ae43fe3a3f26
    Signed-off-by: Matthieu Mauger <matthieux.mauger@intel.com>
    Reviewed-on: http://android.intel.com:8080/100635
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Samoun, JacquesX <jacquesx.samoun@intel.com>
    Reviewed-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: De Cesco, Jonathan <jonathan.de.cesco@intel.com>
    Tested-by: Zurmely, PierreX <pierrex.zurmely@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- src/com/android/settings/hotspot/HotspotSettings.java
+++ src/com/android/settings/hotspot/HotspotSettings.java
@@ -33,6 +33,7 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -280,6 +281,8 @@ public final class HotspotSettings extends SettingsPreferenceFragment
                 }
                 mMyDevicePreference.setSummary(mWifiConfig.SSID);
             }
+            Settings.System.putInt(getContentResolver(), mDialog.KEY_HOTSPOT_SOUND_NOTIFY,
+                    mDialog.isSoundNotifyEnabled() ? 1: 0);
         }
     }
 
