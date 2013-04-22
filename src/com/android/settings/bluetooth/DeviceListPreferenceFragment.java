eb3f33162b0a445bcdef9cbb7a6e5341f3bf32d1
jshe32X <jianchunx.shen@intel.com>
2013-04-22 18:11:52 +0800

    [PORT FROM MAIN]Settings: Fix NullPointerException in DeviceListPreferenceFragment.java
    BZ: 100659
    
    RootCause: Check null pointer when using mDeviceListGroup.
    
    Category: AOSP Improvement
    Domain: AOSP-Settings
    Origin: internal
    Upstream-Candidate: no
    
    Orig-Change-Id: Ib2bdc39e9661042732ab1c1c89591884cd91121a
    
    Change-Id: Iaed5be424173da7d8a3e0e11f20dedbd00fe68d1
    Signed-off-by: jshe32X<jianchunx.shen@intel.com>

--- src/com/android/settings/bluetooth/DeviceListPreferenceFragment.java
+++ src/com/android/settings/bluetooth/DeviceListPreferenceFragment.java
@@ -171,7 +171,9 @@ public abstract class DeviceListPreferenceFragment extends
                 getActivity(), cachedDevice);
 
         initDevicePreference(preference);
-        mDeviceListGroup.addPreference(preference);
+        if (mDeviceListGroup != null) {
+            mDeviceListGroup.addPreference(preference);
+        }
         mDevicePreferenceMap.put(cachedDevice, preference);
     }
 
