3961e21a6ecd5f4a227cf82bba97f742020f32fd
charitardha <satya.charitardha.jayanti@intel.com>
2014-01-21 16:50:56 +0530

    Media volume(Music sound) change to small when launching settings app.
    BZ: 154458
    
    When Home button is pressed in Settings->Sound->Volumes, old media
    volume value is cleaned up in this patch. So, when Settings app is
    opened again, the current volume value set by the hardware keys is
    taken to initialize the media volume bar in the Volumes dialog and
    there wont be any media volume change observed.
    
    Change-Id: Ibc8235c87cf555067d6e1382c151cb9823b9da39
    Category: aosp improvement
    Domain: AUDIO_MEDIA-External
    Origin: internal
    Upstream-Candidate: yes
    Signed-off-by: charitardha <satya.charitardha.jayanti@intel.com>

--- src/com/android/settings/RingerVolumePreference.java
+++ src/com/android/settings/RingerVolumePreference.java
@@ -259,6 +259,7 @@ public class RingerVolumePreference extends VolumePreference {
         for (SeekBarVolumizer vol : mSeekBarVolumizer) {
             if (vol != null) vol.stopSample();
         }
+        cleanup();
     }
 
     @Override
